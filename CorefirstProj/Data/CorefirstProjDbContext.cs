d6fe34b8eafb6bf6c4854c97d9a9cd44121f7ea6
SharmaNidhiP <115006056+SharmaNidhiP@users.noreply.github.com>
2023-01-31 23:52:23 +0530

    Add files via upload

---
+++ CorefirstProj/Data/CorefirstProjDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CorefirstProj.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace CorefirstProj.Data
+{
+    public class CorefirstProjDbContext:DbContext
+    {
+        public CorefirstProjDbContext(DbContextOptions<CorefirstProjDbContext> options) : base(options)
+            {
+            }
+    public DbSet<Region> Regions { get; set; }
+    public DbSet<Walk> Walks { get; set; }
+    public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    
+    }
+}
